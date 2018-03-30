4d56cf8c4759661e9fc1d79d60b1e83c5981f01b
Agnel Waghela <agnelwaghela@gmail.com>
2018-03-30 12:17:04 +0530

    - :hammer: Update Spec

--- spec/webceo_spec.rb
+++ spec/webceo_spec.rb
@@ -2,10 +2,6 @@ require "spec_helper"
 
 RSpec.describe Webceo do
   it "has a version number" do
-    expect(Webceo::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
+    expect(subject.gem_version).not_to be nil
   end
 end
