65bcd655a701e98de85060df141d49d418c935b6
Agnel Waghela <agnelwaghela@gmail.com>
2018-03-30 12:16:06 +0530

    - :hammer: Update webceo.rb to require all our API classes

--- lib/webceo.rb
+++ lib/webceo.rb
@@ -1,5 +1,12 @@
-require "webceo/version"
+require 'webceo/version'
 
-module Webceo
-  # Your code goes here...
-end
+require 'httparty'
+require 'multi_json'
+require 'active_support/core_ext/hash/indifferent_access'
+require 'webceo/api'
+require 'webceo/api/request'
+require 'webceo/api/response'
+require 'webceo/api/error'
+require 'webceo/api/client'
+
+module Webceo; end # end of module Webceo
