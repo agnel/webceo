55d8ee954e3ed305ddc5d5996a70a2f3b1b45097
Agnel Waghela <agnelwaghela@gmail.com>
2018-04-03 13:16:24 +0530

    - :hammer: Update Gem Version to 1.0.0

--- lib/webceo/version.rb
+++ lib/webceo/version.rb
@@ -1,17 +1,26 @@
 # frozen_string_literal: true
 
 module Webceo
-  # Returns the version of the currently loaded Rails as a <tt>Gem::Version</tt>
+  #
+  # Returns the version of the currently loaded Webceo as a <tt>Gem::Version</tt>
+  #
   def self.gem_version
     Gem::Version.new VERSION::STRING
   end
 
+  #
+  # Module VERSION provides constants as per the semantic versioning specification
+  #
+  # @see https://semver.org/
+  #
+  # @author Agnel Waghela <agnelwaghela@gmail.com>
+  #
   module VERSION
-    MAJOR = 0
-    MINOR = 1
-    TINY  = 0
+    MAJOR = 1
+    MINOR = 0
+    PATCH = 0
     PRE   = nil # 'alpha', 'beta', 'rc1', 'rc2', nil
 
-    STRING = [MAJOR, MINOR, TINY, PRE].compact.join(".")
+    STRING = [MAJOR, MINOR, PATCH, PRE].compact.join(".")
   end
 end
