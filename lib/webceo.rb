6c117ff83a6d92e7a28db9a9948e607c81e3a853
Agnel Waghela <agnelwaghela@gmail.com>
2018-04-03 12:29:32 +0530

    - :sparkles: Add methods for configuration in Webceo module - :hammer: Move require statements for classes under api to module api

--- lib/webceo.rb
+++ lib/webceo.rb
@@ -4,9 +4,21 @@ require 'httparty'
 require 'multi_json'
 require 'active_support/core_ext/hash/indifferent_access'
 require 'webceo/api'
-require 'webceo/api/request'
-require 'webceo/api/response'
-require 'webceo/api/error'
-require 'webceo/api/client'
+require 'webceo/configuration'
 
-module Webceo; end # end of module Webceo
+module Webceo
+
+  class << self
+    attr_accessor :configuration
+  end
+
+  def self.configure
+    self.configuration ||= Webceo::Configuration.new
+    yield(configuration)
+  end
+
+  def self.reset
+    self.configuration = Webceo::Configuration.new
+  end
+
+end # end of module Webceo
